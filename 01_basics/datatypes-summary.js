28711e2b4c2f373a6f87f827065fc52fc878e5cc
arjunfoc <121789855+arjunfoc@users.noreply.github.com>
2024-04-21 18:33:19 +0000

    summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitve
+
+// 7 types : String , Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+// const bigNumber = 1234555557605n
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "abhishek",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+
+console.log(typeof scoreValue)
+
+
+
+
+
+
+
+
+
